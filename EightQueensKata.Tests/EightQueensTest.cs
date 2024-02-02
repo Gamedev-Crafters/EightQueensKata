cd13a1d54c939f071667af07037532ea5ec5009d
Javierdds <deduenassantanojavier@gmail.com>
2024-02-02 14:52:40 +0100

    build: first commit

---
+++ EightQueensKata.Tests/EightQueensTest.cs
@@ -0,0 +1,15 @@
+/*
+ * 1. Place 8 chess queens on a 8x8 chessboard
+ * 2. None of them should be able to capture any other
+ */
+
+namespace EightQueensKata.Tests;
+
+public class EightQueensTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ EightQueensKata/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace EightQueensKata;
+
+public class Class1
+{
+}
