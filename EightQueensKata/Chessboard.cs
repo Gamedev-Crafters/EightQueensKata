415992a5eab6ec93042390a336ce73736e9e8ca4
Javierdds <deduenassantanojavier@gmail.com>
2024-02-03 18:51:06 +0100

    feat: is another queen in range also checks for diagonal range

--- EightQueensKata/Chessboard.cs
+++ EightQueensKata/Chessboard.cs
@@ -19,14 +19,14 @@ public class Chessboard
 
     public void Initialize()
     {
-        PlaceQueenAt(0,0);
-        PlaceQueenAt(1,0);
-        PlaceQueenAt(2,0);
+        PlaceQueenAt(0,5);
+        PlaceQueenAt(1,3);
+        PlaceQueenAt(2,6);
         PlaceQueenAt(3,0);
-        PlaceQueenAt(4,0);
-        PlaceQueenAt(5,0);
-        PlaceQueenAt(6,0);
-        PlaceQueenAt(7,0);
+        PlaceQueenAt(4,7);
+        PlaceQueenAt(5,1);
+        PlaceQueenAt(6,4);
+        PlaceQueenAt(7,2);
     }
 
     public int QueensCount()
@@ -63,18 +63,54 @@ public class Chessboard
 
     private bool IsAnotherQueenInRange(int xCoordinate, int yCoordinate)
     {
+        if (CheckForDiagonalRange(xCoordinate, yCoordinate)) return true;
+
+        if (CheckForHorizontalAndVerticalRange(xCoordinate, yCoordinate)) return true;
+        
+        return false;
+    }
+
+    private bool CheckForDiagonalRange(int xCoordinate, int yCoordinate)
+    {
+        // Verificar la diagonal superior derecha
+        for (int x = xCoordinate - 1, y = yCoordinate + 1; x >= 0 && y < _board.GetLength(1); x--, y++)
+        {
+            if (x == xCoordinate && y == yCoordinate) continue;
+            if (IsQueenAt(x, y))
+            {
+                return true;
+            }
+        }
+
+        // Verificar la diagonal superior izquierda
+        for (int x = xCoordinate - 1, y = yCoordinate - 1; x >= 0 && y >= 0; x--, y--)
+        {
+            if (x == xCoordinate && y == yCoordinate) continue;
+            if (IsQueenAt(x, y))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool CheckForHorizontalAndVerticalRange(int xCoordinate, int yCoordinate)
+    {
         for (var x = 0; x < _board.GetLength(0); x++)
         {
-            if(x == xCoordinate) continue;
+            if (x == xCoordinate) continue;
             if (IsQueenAt(x, yCoordinate))
                 return true;
         }
+
         for (var y = 0; y < _board.GetLength(0); y++)
         {
-            if(y == yCoordinate) continue;
+            if (y == yCoordinate) continue;
             if (IsQueenAt(xCoordinate, y))
                 return true;
         }
+
         return false;
     }
 }
